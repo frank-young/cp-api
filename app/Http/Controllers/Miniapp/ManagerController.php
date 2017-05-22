8fa4f5d6f7c61b63e40f7892ad2b64840bc3e9cf
frank-young <yangjunalns@qq.com>
2017-05-22 21:34:15 +0800

    .

--- app/Http/Controllers/Miniapp/ManagerController.php
+++ app/Http/Controllers/Miniapp/ManagerController.php
@@ -3,6 +3,7 @@ namespace App\Http\Controllers\Miniapp;
 
 use App\Models\Session;
 use App\Models\Term;
+use App\Models\Taskahead;
 use App\Models\Questionterm;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -42,16 +43,25 @@ class ManagerController extends Controller
     public function termStatus(Request $request)
     {
       $openid = Session::getOpenid($request->input('session_key'));
-      $term = Term::where(['id'=>1])->first();
-      if($term->status == 1) {
-        $question = Questionterm::where(['openid' =>$openid])->first();
-        if (empty($question)) {
-          $res = returnCode(true,'立即报名',1);
-        } else {
-          $res = returnCode(true,'修改资料',2);
-        }
+      $task = Taskahead::where(['openid' => $openid])->first();
+      if (!empty($task)) {
+        $res = returnCode(true,'查看任务',5);
       } else {
-        $res = returnCode(false,'匹配查询',0);
+        $term = Term::where(['id'=>1])->first();
+        if($term->status == 1) {
+          $question = Questionterm::where(['openid' =>$openid])->first();
+          if (empty($question)) {
+            $res = returnCode(true,'立即报名',1);
+          } else {
+            if ($question->is_share == 1) {
+              $res = returnCode(true,'修改资料',2);
+            } else {
+              $res = returnCode(true,'立即分享',3);
+            }
+          }
+        } else {
+          $res = returnCode(true,'匹配查询',4);
+        }
       }
       return response()->json($res);
     }
